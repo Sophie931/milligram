8975557cfbbe19d0e54eaed894bc1afb86773a29
unknown <fortseclin@gmail.com>
2020-01-09 14:12:28 +0100

    VmHeading

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,13 @@
   <p><em>Yeah!! Milligram is amazing.</em></p>
 </blockquote>
 
-Les boutons :
-<a class="button" href="#">Default Button</a>
-
-<button class="button button-outline">Outlined Button</button>
-
-<input class="button button-clear" type="submit" value="Clear Button">
+Les titres :
+<VmHeading type="1" texte="Mon titre 1"></VmHeading> equivalent <h1>Mon titre 1</h1>
+<VmHeading type="2" texte="Mon titre 2"></VmHeading> equivalent <h2>Mon titre 2</h2>
+<VmHeading type="3" texte="Mon titre 3"></VmHeading> equivalent <h3>Mon titre 3</h3>
+<VmHeading type="4" texte="Mon titre 4"></VmHeading> equivalent <h4>Mon titre 4</h4>
+<VmHeading type="5" texte="Mon titre 5"></VmHeading> equivalent <h5>Mon titre 5</h5>
+<VmHeading type="6" texte="Mon titre 6"></VmHeading> equivalent <h6>Mon titre 6</h6>
 
   </div>
 </template>
@@ -21,11 +22,15 @@ Les boutons :
 <script>
 // 1 - j'importe
 import VmBlockquote from './components/VmBlockquote'
+import VmHeading from './components/VmHeading'
 // 2  je déclare
 export default {
   name: 'app',
   // j'injecte le VMBlockquote après l'avoir importé
-  components: {VmBlockquote}  
+  components: {
+    VmBlockquote,
+    VmHeading
+  },  
 }
 </script>
 
---
+++ src/components/VmHeading.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+ <!--
+    <h1 v-if="type==='1'">{{texte}}</h1>
+  <h2 v-else-if="type==='2'">{{texte}}</h2>
+  <h3 v-else-if="type==='3'">{{texte}}</h3>
+  <h4 v-else-if="type==='4'">{{texte}}</h4>
+  <h5 v-else-if="type==='5'">{{texte}}</h5>
+  <h6 v-else-if="type==='6'">{{texte}}</h6>
+  -->
+  <component :is="elementAAfficher">{{texte}}</component>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'VmHeading',
+    props: [ 'type', 'texte'],
+    computed: {
+      elementAAfficher () {
+        return `h${this.type}`
+        //peut aussi s'écrire
+        //return "h" + this.type
+      }
+    }
+  
+}
+
+
+</script>
+
+<style>
+
+</style>
