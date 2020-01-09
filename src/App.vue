e1984595cc15ddbbc54859658ceeeed6fd08c8c6
unknown <fortseclin@gmail.com>
2020-01-09 10:31:07 +0100

    composant blockquote

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+   appel du component avec une props
+    <!-- 3 - j'utilise -->
+  <VmBlockquote text="Mon texte"></VmBlockquote>
+  équivalent à
+<blockquote>
+  <p><em>Yeah!! Milligram is amazing.</em></p>
+</blockquote>
+
+Les boutons :
+<a class="button" href="#">Default Button</a>
+
+<button class="button button-outline">Outlined Button</button>
+
+<input class="button button-clear" type="submit" value="Clear Button">
+
+  </div>
+</template>
+
+<script>
+// 1 - j'importe
+import VmBlockquote from './components/VmBlockquote'
+// 2  je déclare
+export default {
+  name: 'app',
+  // j'injecte le VMBlockquote après l'avoir importé
+  components: {VmBlockquote}  
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/VmBlockquote.vue
@@ -0,0 +1,18 @@
+<template>
+  <blockquote>
+  <p><em>{{ text }}</em></p>
+</blockquote>
+</template>
+
+<script>
+// je déclare la propos qui sera utilisé dans appvue
+export default {
+    name: 'VmBlockquote',
+    props:['text']
+
+}
+</script>
+
+<style>
+
+</style>
